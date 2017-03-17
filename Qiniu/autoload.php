fb010ea35608cbacc51be6895d7bff4f7fd68cab
leaf <js>
2017-03-17 11:25:10 +0800

    init

--- Qiniu/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('QINIUROOT', str_replace('Qiniu', '', __DIR__));
-
-spl_autoload_register(function($class) {
-    include QINIUROOT . str_replace("\\",'/',$class) . '.php';
-});
-
-include 'functions.php';
